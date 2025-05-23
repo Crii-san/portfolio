922eb1c3d8390c1af2eb064d296cacfa52ca0877
Christelle Souka <soukachristelle@gmail.com>
2025-05-23 15:37:03 +0200

    Installation PrimeNG, ajout d'un footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, UpperCasePipe],
+  standalone: true,
+  imports: [CommonModule, UpperCasePipe, ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
