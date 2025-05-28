27e64f0e4415b8ae49b66ae5a1f8c03db55bebf5
Christelle Souka <soukachristelle@gmail.com>
2025-05-28 22:43:35 +0200

    Gestion de l'apparition du burger menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./containers/header/header.component";
 import { MainComponent } from "./containers/main/main.component";
 import { FooterComponent } from "./containers/footer/footer.component";
+import { BurgerMenuComponent } from "./burger-menu/burger-menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent, BurgerMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+
+  public showBurgerMenu: boolean = false;
+
+  public closeBurgerMenu(): void {
+      this.showBurgerMenu = false;
+  }
 }
