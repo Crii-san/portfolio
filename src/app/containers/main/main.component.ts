5866990f3a198a5dd8040bf151d2256acbd89ef8
Christelle Souka <soukachristelle@gmail.com>
2025-06-07 15:47:49 +0200

    Ajout de toutes les sections

--- src/app/containers/main/main.component.ts
+++ src/app/containers/main/main.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CompetencesComponent } from "../../sections/competences/competences.component";
 import { ContactComponent } from "../../sections/contact/contact.component";
+import { ProjetsComponent } from "../../sections/projets/projets.component";
+import { ExperiencesComponent } from "../../sections/experiences/experiences.component";
+import { EtudesComponent } from "../../sections/etudes/etudes.component";
+import { PresentationComponent } from "../../sections/presentation/presentation.component";
 
 @Component({
   selector: 'app-main',
-  imports: [CompetencesComponent, ContactComponent],
+  imports: [CompetencesComponent, ContactComponent, ProjetsComponent, ExperiencesComponent, EtudesComponent, PresentationComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
