e08483da6b78c58f753b127557246318204e405e
Christelle Souka <soukachristelle@gmail.com>
2025-06-07 10:21:19 +0200

    Affichage des moyens de contact avec les liens

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,38 @@
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Contact {
+  type: string,
+  link: string,
+  croppedLink: string,
+  icon: string|null
+}
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [NgFor, NgIf, UpperCasePipe],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  public contactModes: Contact[] = [
+    {
+      type: 'linkedin',
+      link: 'https://www.linkedin.com/in/christelle-souka/',
+      croppedLink: 'linkedin.com/in/christelle-souka/',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1024px-LinkedIn_icon.svg.png'
+    },
+    {
+      type: 'email',
+      link: 'mailto:soukachristelle@gmail.com',
+      croppedLink: 'soukachristelle@gmail.com',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png'
+    },
+    {
+      type: 'github',
+      link: 'https://github.com/Crii-san',
+      croppedLink: 'github.com/Crii-san',
+      icon: 'https://cdn-icons-png.flaticon.com/512/25/25231.png'
+    }
+  ];
 }
--- src/app/containers/main/main.component.ts
+++ src/app/containers/main/main.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CompetencesComponent } from "../../competences/competences.component";
+import { ContactComponent } from "../../contact/contact.component";
 
 @Component({
   selector: 'app-main',
-  imports: [CompetencesComponent],
+  imports: [CompetencesComponent, ContactComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-  public test() {
-    console.log('coucou');
-  }
 }
