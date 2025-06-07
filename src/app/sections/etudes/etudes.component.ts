f27a7f62becf778996120e0e2bac0ec6cd964f09
Christelle Souka <soukachristelle@gmail.com>
2025-06-07 15:46:19 +0200

    Section études

---
+++ src/app/sections/etudes/etudes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-etudes',
+  imports: [],
+  templateUrl: './etudes.component.html',
+  styleUrl: './etudes.component.css'
+})
+export class EtudesComponent {
+
+}
