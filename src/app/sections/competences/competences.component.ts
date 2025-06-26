588057ba638306da76826b0f9ee1c66f5dd99ea7
Christelle Souka <soukachristelle@gmail.com>
2025-06-26 20:20:51 +0200

    Section compétences : terminé

--- src/app/sections/competences/competences.component.ts
+++ src/app/sections/competences/competences.component.ts
@@ -108,5 +108,84 @@ export class CompetencesComponent {
       logo: "assets/competences/frameworks/godot.png",
       link: "https://godotengine.org/fr/",
     },
+  ];
+  public database: technology[] = [
+    {
+      name: "Oracle",
+      logo: "assets/competences/database/oracle.png",
+      link: "https://www.oracle.com/",
+    },
+    {
+      name: "MySQL",
+      logo: "assets/competences/database/mysql.png",
+      link: "https://www.mysql.com/fr/",
+    },
+    {
+      name: "SQLite",
+      logo: "assets/competences/database/sqlite.png",
+      link: "https://www.sqlite.org/",
+    }, 
+    {
+      name: "MongoDB",
+      logo: "assets/competences/database/mongodb.webp",
+      link: "https://www.mongodb.com/",
+    },
+    {
+      name: "PostgreSQL",
+      logo: "assets/competences/database/postgresql.png",
+      link: "https://www.postgresql.org/",
+    },
+    {
+      name: "Doctrine",
+      logo: "assets/competences/database/doctrine.png",
+      link: "https://www.doctrine-project.org/",
+    },
+    {
+      name: "Prisma",
+      logo: "assets/competences/database/prisma.jpg",
+      link: "https://www.prisma.io/",
+    },
+  ];
+  public divers: technology[] = [
+    {
+      name: "Docker",
+      logo: "assets/competences/divers/docker.webp",
+      link: "https://www.docker.com/",
+    },
+    {
+      name: "PrimeNG",
+      logo: "assets/competences/divers/primeng.png",
+      link: "https://primeng.org/",
+    },
+    {
+      name: "VSCode",
+      logo: "assets/competences/divers/vscode.png",
+      link: "https://code.visualstudio.com/",
+    },
+    {
+      name: "PhpStorm",
+      logo: "assets/competences/divers/phpstorm.png",
+      link: "https://www.jetbrains.com/fr-fr/phpstorm/",
+    },
+    {
+      name: "Visual Studio",
+      logo: "assets/competences/divers/vs.png",
+      link: "https://code.visualstudio.com/",
+    },
+    {
+      name: "Eclipse",
+      logo: "assets/competences/divers/eclipse.webp",
+      link: "https://eclipseide.org/",
+    },
+    {
+      name: "GitLab",
+      logo: "assets/competences/divers/gitlab.png",
+      link: "https://about.gitlab.com/fr-fr/free-trial/devsecops/?utm_medium=cpc&utm_source=google&utm_campaign=gitlab_search_fr&utm_content=fr_br_gitlab_core&utm_term=gitlab&_bt=692837190945&_bk=gitlab&_bm=e&_bn=g&_bg=158296160046&gad_source=1&gad_campaignid=21074610372&gbraid=0AAAAADcJCbfuD-DrWDCIATEpo_q49THbb&gclid=CjwKCAjw3_PCBhA2EiwAkH_j4qG7k8g6OrvlMp-TWXtFDzUxhIFbKSA3to54rcXXzlL00k6T8sv9IRoCC-gQAvD_BwE",
+    },
+    {
+      name: "GitHub",
+      logo: "assets/competences/divers/github.png",
+      link: "https://github.com/",
+    },
   ]
 }
