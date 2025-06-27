b9e2b928a91ed5242d716d3dd6c8d25372d877dc
Christelle Souka <soukachristelle@gmail.com>
2025-06-27 10:22:54 +0200

    Installation primeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,20 @@ import { provideRouter } from '@angular/router';
 import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideAnimationsAsync(),  
+    provideAnimationsAsync(), 
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.dark-mode'
+        }
+      }
+    }) 
   ]
 };
