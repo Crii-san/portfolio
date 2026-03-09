6346fb5fdd3f34712266bbb1513cd3c23d0743fa
Christelle Souka <soukachristelle@gmail.com>
2026-03-09 11:59:05 +0100

    style: new design for home component

--- src/app/components/main/home/home.component.ts
+++ src/app/components/main/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Button } from "primeng/button";
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Button, TooltipModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
